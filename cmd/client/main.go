58a1e3f6c30141f03243988b069020db77d24312
George Tsetsegmaa <macdev@macdev-2.local>
2026-05-10 12:00:37 -0700

    Add rabbitmq auto queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("could not get client username")
+	}
+
+	
 }
---
+++ internal/pubsub/publish.go
@@ -0,0 +1,47 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        data,
+	})
+}
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not create channel: %v", err)
+	}
+	
+	queue, err := ch.QueueDeclare(queueName, queueType == SimpleQueueDurable, queueType == SimpleQueueTransient, queueType == SimpleQueueTransient, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare a queue: %v", err)
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind to queue: %v", err)
+	}
+
+	return ch, queue, nil
+}
