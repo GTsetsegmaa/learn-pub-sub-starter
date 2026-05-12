32f244ad2c7e35f63c30da28c9ccdd2aa40976f2
George Tsetsegmaa <macdev@macdev-2.local>
2026-05-12 00:13:00 -0700

    Add routing pattern

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -26,9 +26,18 @@ func main() {
 		log.Fatalf("could not get client username")
 	}
 
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not open channel: %v", err)
+	}
+
 	queueName := routing.PauseKey + "." + username
 	gs := gamelogic.NewGameState(username)
 	pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.SimpleQueueTransient, handlerPause(gs))
+
+	moveQueueName := routing.ArmyMovesPrefix + "." + username
+	moveBindingKey := routing.ArmyMovesPrefix + ".*"
+	pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, moveQueueName, moveBindingKey, pubsub.SimpleQueueTransient, handlerMove(gs))
 
 	for {
 		words := gamelogic.GetInput()
@@ -44,11 +53,17 @@ func main() {
 				continue
 			}
 		case "move":
-			_, err = gs.CommandMove(words)
+			armyMove, err := gs.CommandMove(words)
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilTopic, moveQueueName, armyMove)
+			if err != nil {
+				log.Printf("could not publish move: %v", err)
+				continue
+			}
+			fmt.Println("chat message published successfully")
 		case "status":
 			gs.CommandStatus()
 		case "help":
@@ -68,5 +83,12 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(gl gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(gl)
 	}
 }
